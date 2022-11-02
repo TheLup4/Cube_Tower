650d4cf7ebe60604d57db71396e79b8cb383d30f
TheLup4 <102719479+TheLup4@users.noreply.github.com>
2022-11-02 20:52:06 +0500

    Игра

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public Sprite musicOn, musicOff;
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name =="Sound")
+        {
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+    public void RestartGame()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+        {
+            GetComponent<AudioSource>().Play();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void LoadShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+            SceneManager.LoadScene("Shop");
+    }
+    public void CloseShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene("Main");
+    }
+    public void LoadVK()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+        Application.OpenURL("https://vk.com/trapeznikovk");
+    }
+    public void MusicWork()
+    {
+        //Музыка выключена, нужно включить
+        if (PlayerPrefs.GetString("music") == "No")
+        {
+            GetComponent<AudioSource>().Play();
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+        }
+        else
+        {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+
+    }
+}
